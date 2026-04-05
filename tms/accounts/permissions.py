d3a558f058281227e90b4ecfbe0e54ce3d46b9e9
Aman <aman08.stars@gmail.com>
2026-04-06 02:04:04 +0530

    update

---
+++ tms/accounts/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import BasePermission
+
+
+class RolePermission(BasePermission):
+    allowed_roles = ()
+
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(
+            user
+            and user.is_authenticated
+            and getattr(user, "role", None) in self.allowed_roles
+        )
+
+
+class IsAdmin(RolePermission):
+    allowed_roles = ("ADMIN",)
+
+
+class IsManager(RolePermission):
+    allowed_roles = ("MANAGER",)
+
+
+class IsTrainer(RolePermission):
+    allowed_roles = ("TRAINER",)
+
+
+class IsManagerOrTrainer(RolePermission):
+    allowed_roles = ("MANAGER", "TRAINER")
