6cb7c410f44f46fd290570e1db7f0824adfa3c79
Aman <aman08.stars@gmail.com>
2026-04-11 15:37:03 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ACCESS_TOKEN_KEY, clearStoredAuth } from "../auth/storage";
 
 const API = axios.create({
-  baseURL: "https://tms.up.railway.app",
+  baseURL: "https://tms.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
