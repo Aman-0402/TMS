4d03dba456c288d30efce2dc7bbc1b3e975c7c0c
Aman <aman08.stars@gmail.com>
2026-04-12 15:04:31 +0530

    update

--- tms/accounts/apps.py
+++ tms/accounts/apps.py
@@ -3,3 +3,19 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        import os
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        # Prevent duplicate creation
+        if not User.objects.filter(username="admin").exists():
+            print("🔥 Creating default superuser...")
+
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
