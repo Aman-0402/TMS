834075bc607aaf33a95d63dbb99a47f425cfc1e6
Aman <aman08.stars@gmail.com>
2026-04-12 10:03:42 +0530

    u

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source /opt/venv/bin/activate
-cd tms
-gunicorn tms.wsgi
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Starting server..."
+gunicorn tms.wsgi:application --bind 0.0.0.0:$PORT
